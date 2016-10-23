81516cdfec8e25b0dd949c86c5909d7c5a8c2b0a
Mark Halls <fireraisr@gmail.com>
2016-10-23 16:07:01 -0700

    Kata Finished, works with any size int

--- Roman_Nums/RN_2016-10-23/src/com/romannumeral/KataApp.java
+++ Roman_Nums/RN_2016-10-23/src/com/romannumeral/KataApp.java
@@ -4,7 +4,11 @@ package com.romannumeral;
 public class KataApp {
 	//--------------------------------------------------------------------------
 	public static void main( String[] args ) {
-		
+		RomanNumeral numeral = new RomanNumeral();
+		System.out.println(numeral.set( 1999 ).toString() );
+		RomanNumeral num = new RomanNumeral();
+		System.out.println(num.set( 23331 ).toString() );
+
 	}
 	//--------------------------------------------------------------------------
 }
---
+++ Roman_Nums/RN_2016-10-23/src/com/romannumeral/RomanNumeral.java
@@ -0,0 +1,68 @@
+package com.romannumeral;
+
+//==============================================================================
+public class RomanNumeral {
+	//--------------------------------------------------------------------------
+	private int number;
+	private StringBuilder builder = new StringBuilder();
+
+	//--------------------------------------------------------------------------
+
+	//--------------------------------------------------------------------------
+	public RomanNumeral set( int number ) {
+		this.number = number;
+		return this;
+	}
+	//--------------------------------------------------------------------------
+
+	//--------------------------------------------------------------------------
+	public void stringFormatter( int magnitude, String tens, String fivs, String ones ) {
+		int position = (int) (number % ( magnitude * 10f ) / magnitude);
+
+		switch ( position ) {
+			case 1:
+				builder.append( ones );
+				break;
+			case 2:
+				builder.append( ones ).append( ones );
+				break;
+			case 3:
+				builder.append( ones ).append( ones ).append( ones );
+				break;
+			case 4:
+				builder.append( ones ).append( fivs );
+				break;
+			case 5:
+				builder.append( fivs );
+				break;
+			case 6:
+				builder.append( fivs ).append( ones );
+				break;
+			case 7:
+				builder.append( fivs ).append( ones ).append( ones );
+				break;
+			case 8:
+				builder.append( fivs ).append( ones ).append( ones ).append( ones );
+				break;
+			case 9:
+				builder.append( ones ).append( tens );
+				break;
+		}
+	}
+	//--------------------------------------------------------------------------
+
+	//--------------------------------------------------------------------------
+	@Override public String toString() {
+		while( number > 1000 ) {
+			builder.append( "M" );
+			number -= 1000;
+		}
+		stringFormatter( 100, "M", "D", "C" );
+		stringFormatter( 10, "C", "L", "X" );
+		stringFormatter( 1, "X", "V", "I" );
+		return builder.toString();
+	}
+
+
+}
+//------------------------------------------------------------------------------
