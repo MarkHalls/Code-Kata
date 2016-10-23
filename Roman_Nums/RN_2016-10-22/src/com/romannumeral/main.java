71e716d1dc48cd349b20f85d9e120e3b8dd59f75
Mark Halls <fireraisr@gmail.com>
2016-10-22 20:15:04 -0700

    Get keyboard input

--- Roman_Nums/RN_2016-10-22/src/com/romannumeral/main.java
+++ Roman_Nums/RN_2016-10-22/src/com/romannumeral/main.java
@@ -1,10 +1,14 @@
 package com.romannumeral;
 
+import java.util.Scanner;
+
 //==============================================================================
 public class main {
 	//--------------------------------------------------------------------------
 	public static void main( String[] args ) {
-		
+		Scanner keyboard = new Scanner( System.in );
+		int intToConvert = keyboard.nextInt();
+		System.out.println(intToConvert);
 	}
 	//--------------------------------------------------------------------------
 }
