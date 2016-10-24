102067aa13d8dc3683ba447a6e83abd493c2ce07
firesetter <fireraisr@gmail.com>
2016-10-24 14:08:15 -0700

    Started Kata

---
+++ Roman_Nums/RN_2016-10-24/src/com/romannumeral/KataApp.java
@@ -0,0 +1,10 @@
+package com.romannumeral;
+
+/**
+ * Created by mh353f on 10/24/2016.
+ */
+public class KataApp {
+    public static void main(String[] args) {
+        System.out.println( new RomanNumeral(1987).toString() );
+    }
+}
---
+++ Roman_Nums/RN_2016-10-24/src/com/romannumeral/RomanNumeral.java
@@ -0,0 +1,8 @@
+package com.romannumeral;
+
+/**
+ * Created by mh353f on 10/24/2016.
+ */
+public class RomanNumeral {
+    
+}
