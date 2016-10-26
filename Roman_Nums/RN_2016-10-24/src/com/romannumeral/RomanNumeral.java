235231c4f847d83c2421ab1b81d64dc80e04aa1a
firesetter <fireraisr@gmail.com>
2016-10-26 14:56:03 -0700

    switched to String.valueOf to get characters. switch statement still broken

--- Roman_Nums/RN_2016-10-24/src/com/romannumeral/RomanNumeral.java
+++ Roman_Nums/RN_2016-10-24/src/com/romannumeral/RomanNumeral.java
@@ -60,46 +60,62 @@ public class RomanNumeral {
         int tempNumber = 0;
         for (int position = 0; position < numeral.length(); position++) {
             if ((numeral.length() - position) >= 2) {
-                String tempNumeral = String.valueOf(( numeral.charAt(position) + numeral.charAt(position + 1)));
-                System.out.println(tempNumeral);
+                String tempNumeral = String.valueOf( numeral.charAt(position)) + String.valueOf(numeral.charAt(position + 1));
                 switch ( tempNumeral ){
                     case "CM":
+                        System.out.println(".CM");
                         tempNumber += 900;
+                        position++;
                         break;
                     case "CD":
+                        System.out.println(".CD");
                         tempNumber += 400;
+                        position++;
                         break;
                     case "LC":
+                        System.out.println(".LC");
                         tempNumber += 90;
+                        position++;
                         break;
                     case "XL":
+                        System.out.println(".XL");
                         tempNumber += 40;
+                        position++;
                         break;
                     case "IX":
+                        System.out.println(".IX");
                         tempNumber += 9;
+                        position++;
                         break;
                     default:
                         tempNumeral = String.valueOf(numeral.charAt(position));
                         switch (tempNumeral) {
                             case "M":
+                                System.out.println( ".M");
                                 tempNumber += 1000;
                                 break;
                             case "D":
+                                System.out.println( ".D");
                                 tempNumber += 500;
                                 break;
                             case "C":
+                                System.out.println( ".C" );
                                 tempNumber += 100;
                                 break;
                             case "L":
+                                System.out.println( ".L" );
                                 tempNumber += 50;
                                 break;
                             case "X":
+                                System.out.println( ".X" );
                                 tempNumber += 10;
                                 break;
                             case "V":
+                                System.out.println( "V" );
                                 tempNumber += 5;
                                 break;
                             case "I":
+                                System.out.println( "I" );
                                 tempNumber += 5;
                                 break;
                             default:
@@ -111,24 +127,31 @@ public class RomanNumeral {
                     String tempNumeral = String.valueOf(numeral.charAt(position));
                     switch (tempNumeral) {
                         case "M":
+                            System.out.println( ".M");
                             tempNumber += 1000;
                             break;
                         case "D":
+                            System.out.println( ".D");
                             tempNumber += 500;
                             break;
                         case "C":
+                            System.out.println( ".C" );
                             tempNumber += 100;
                             break;
                         case "L":
+                            System.out.println( ".L" );
                             tempNumber += 50;
                             break;
                         case "X":
+                            System.out.println( ".X" );
                             tempNumber += 10;
                             break;
                         case "V":
+                            System.out.println( "V" );
                             tempNumber += 5;
                             break;
                         case "I":
+                            System.out.println( "I" );
                             tempNumber += 5;
                             break;
                         default:
