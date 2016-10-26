bb3d7664c42238fb7c59b83a6cc01e4726dbddd5
firesetter <fireraisr@gmail.com>
2016-10-26 13:52:08 -0700

    added test case for override toString

--- Roman_Nums/RN_2016-10-24/src/com/romannumeral/RomanNumeralTest.java
+++ Roman_Nums/RN_2016-10-24/src/com/romannumeral/RomanNumeralTest.java
@@ -1,14 +1,15 @@
 package com.romannumeral;
 
 import static org.junit.Assert.*;
+import org.junit.Test;
 
 /**
  * Created by mh353f on 10/24/2016.
  */
 public class RomanNumeralTest {
-    @org.junit.Test
-    public void toString() throws Exception {
-
+    @Test
+    public void toStringTest(){
+        assertEquals( "MCMLXXXVII", new RomanNumeral(1987).toString() );
     }
 
 }
